d83e9e58c8d384c8347dee3c80798e20f8ce74e6
Vulcan678 <153300908+Vulcan678@users.noreply.github.com>
2026-02-16 19:56:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ EMAIL = os.getenv("GMAIL_USER")
 PASSWORD = os.getenv("GMAIL_APP_PASSWORD")
 API_KEY = os.getenv("GEMINI_API_KEY")
 
+size = "30W-30L"
+avoid_words = "bootcut, skinny, slim, athletic"
+
 # CONFIG
 URL = "https://www.terminalx.com/men/pants/jeans?product_list_order=price_asc"
 
@@ -15,15 +18,15 @@ URL = "https://www.terminalx.com/men/pants/jeans?product_list_order=price_asc"
 response = requests.get(URL)
 soup = BeautifulSoup(response.text, "html.parser")
 
-content = soup.get_text()[:5000]
+content = soup.get_text()
 
 # Step 2: analyze with Gemini
 genai.configure(api_key=API_KEY)
 
-model = genai.GenerativeModel("gemini-pro")
+model = genai.GenerativeModel("gemini-3-flash-preview")
 
 analysis = model.generate_content(
-    f"Summarize important changes or insights from this page:\n{content}"
+    f"Summarize in array 'url1,url3,url4,...' all the products with avilable size of {size} and without the words {avoid_words} from this page:\n{content}"
 )
 
 result = analysis.text
